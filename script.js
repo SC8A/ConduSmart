c2aa06459c5132cdcaa29f2ea0d7ea4a9b364194
SC8A <sebas.co99@hotmail.com>
2022-06-06 00:32:20 -0500

    Menu Hambuguer y mas responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".header__menu");
+
+hamburguer.addEventListener("click",() => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".header__links").forEach(n => n.addEventListener("click", () => {
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
